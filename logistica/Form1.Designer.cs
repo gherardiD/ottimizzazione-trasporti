881bef1cf311f29d5b84f96bb632e2915b3135e1
gherardiD <gherardi.daniele.studente@itispaleocapa.it>
2023-09-26 12:00:08 +0200

    tabella

--- logistica/Form1.Designer.cs
+++ logistica/Form1.Designer.cs
@@ -32,16 +32,24 @@
             this.produttori_label = new System.Windows.Forms.Label();
             this.consumatori_label = new System.Windows.Forms.Label();
             this.genera_btn = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.panel_table = new System.Windows.Forms.Panel();
+            this.table_default = new System.Windows.Forms.DataGridView();
+            this.productor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price_0 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price_1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price_2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tot_producted = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.consumatori_num)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.produttori_num)).BeginInit();
+            this.panel_table.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.table_default)).BeginInit();
             this.SuspendLayout();
             // 
             // consumatori_num
             // 
             this.consumatori_num.AutoSize = true;
             this.consumatori_num.Location = new System.Drawing.Point(108, 46);
-            this.consumatori_num.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.consumatori_num.Margin = new System.Windows.Forms.Padding(2);
             this.consumatori_num.Minimum = new decimal(new int[] {
             1,
             0,
@@ -60,7 +68,7 @@
             // 
             this.produttori_num.AutoSize = true;
             this.produttori_num.Location = new System.Drawing.Point(108, 23);
-            this.produttori_num.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.produttori_num.Margin = new System.Windows.Forms.Padding(2);
             this.produttori_num.Minimum = new decimal(new int[] {
             1,
             0,
@@ -104,7 +112,7 @@
             this.genera_btn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.genera_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.genera_btn.Location = new System.Drawing.Point(241, 24);
-            this.genera_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.genera_btn.Margin = new System.Windows.Forms.Padding(2);
             this.genera_btn.Name = "genera_btn";
             this.genera_btn.Size = new System.Drawing.Size(102, 39);
             this.genera_btn.TabIndex = 4;
@@ -112,15 +120,61 @@
             this.genera_btn.UseVisualStyleBackColor = false;
             this.genera_btn.Click += new System.EventHandler(this.genera_btn_Click);
             // 
-            // button1
+            // panel_table
             // 
-            this.button1.Location = new System.Drawing.Point(418, 146);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.panel_table.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel_table.Controls.Add(this.table_default);
+            this.panel_table.Location = new System.Drawing.Point(12, 103);
+            this.panel_table.Name = "panel_table";
+            this.panel_table.Size = new System.Drawing.Size(576, 174);
+            this.panel_table.TabIndex = 6;
+            // 
+            // table_default
+            // 
+            this.table_default.CausesValidation = false;
+            this.table_default.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table_default.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.productor,
+            this.price_0,
+            this.price_1,
+            this.price_2,
+            this.tot_producted});
+            this.table_default.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.table_default.Location = new System.Drawing.Point(0, 0);
+            this.table_default.Name = "table_default";
+            this.table_default.RowHeadersWidth = 70;
+            this.table_default.Size = new System.Drawing.Size(576, 174);
+            this.table_default.TabIndex = 6;
+            this.table_default.Visible = false;
+            // 
+            // productor
+            // 
+            this.productor.HeaderText = "productor";
+            this.productor.Name = "productor";
+            this.productor.ReadOnly = true;
+            // 
+            // price_0
+            // 
+            this.price_0.HeaderText = "price";
+            this.price_0.Name = "price_0";
+            // 
+            // price_1
+            // 
+            this.price_1.HeaderText = "price";
+            this.price_1.Name = "price_1";
+            // 
+            // price_2
+            // 
+            this.price_2.HeaderText = "price";
+            this.price_2.Name = "price_2";
+            // 
+            // tot_producted
+            // 
+            this.tot_producted.HeaderText = "tot";
+            this.tot_producted.Name = "tot_producted";
+            this.tot_producted.ReadOnly = true;
             // 
             // logistica_form
             // 
@@ -128,19 +182,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel_table);
             this.Controls.Add(this.genera_btn);
             this.Controls.Add(this.consumatori_label);
             this.Controls.Add(this.produttori_label);
             this.Controls.Add(this.produttori_num);
             this.Controls.Add(this.consumatori_num);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "logistica_form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "logistica";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.consumatori_num)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.produttori_num)).EndInit();
+            this.panel_table.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.table_default)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,7 +209,13 @@
         private System.Windows.Forms.Label produttori_label;
         private System.Windows.Forms.Label consumatori_label;
         private System.Windows.Forms.Button genera_btn;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel_table;
+        private System.Windows.Forms.DataGridView table_default;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price_0;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price_1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price_2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tot_producted;
     }
 }
 
--- logistica/Form1.cs
+++ logistica/Form1.cs
@@ -32,16 +32,39 @@ namespace logistica
 
         private void genera_btn_Click(object sender, EventArgs e)
         {
-            if (check())
+            if (!check())
             {
-                //to do
-                //balblabal
+                this.table_default.Visible = true;
+                //rows
+                for (int r = 0; r < 3; r++)
+                {
+                    this.table_default.Rows.Add("pcd" + r, 30 + r*10, 10 + r * 10, 20 + r * 10, 500);
+                }
+            }
+            else
+            {
+                int numero_produttori = (int)produttori_num.Value;
+                int numero_consumatori = (int)consumatori_num.Value;
+                //creation table
+                DataGridView table = new DataGridView();
+                table.Name= "table";
+                table.Dock = DockStyle.Fill;
+                panel_table.Controls.Add(table);
+                table.ColumnHeadersVisible = true;
+                //columns
+                table.Columns.Add("productor", "Productors");
+                for (int c = 0; c < numero_consumatori; c++)
+                {
+                    table.Columns.Add("price_" + c, "Price");
+                }
+                table.Columns.Add("tot_producted", "tot");
+                //rows
+                for(int r = 0; r < numero_consumatori; r++)
+                {
+                    table.Rows.Add("pcd" + r, 0, 0, 0, 0);
+                }
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("ciao");
-        }
     }
 }
